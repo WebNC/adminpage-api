2be3b86be84427a77affbd6d5733bfbfae2f1eb7
duyminh256 <1612384@student.hcmus.edu.vn>
2019-12-08 12:48:23 +0700

    update student list

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,42 +1,58 @@
-const User = require('../models/users')
+const User = require('../models/users');
 
-exports.getAllUser = async (req,res) =>{
-    const {page} = req.params
-    const pageSize = 25
-    const list = await User.find()
-                            .skip(page*pageSize)
-                            .limit(pageSize)
-    return  res.status(200).send({
-        message: list,
-    });
-}
-exports.getNumberUser = async (req,res) =>{
-    const num = await User.count()
-    return res.status(200).send({
-        message: num,
-    });
-}
+exports.getAllUserTeacher = async (req, res) => {
+  const { page } = req.params;
+  const pageSize = 25;
+  const list = await User.find({ type: 'Người dạy' })
+    .skip(page * pageSize)
+    .limit(pageSize);
+  return res.status(200).send({
+    message: list,
+  });
+};
+exports.getNumberUserTeacher = async (req, res) => {
+  const num = await User.count({ type: 'Người dạy' });
+  return res.status(200).send({
+    message: num,
+  });
+};
 
-exports.getUserDetail = async (req,res)=>{
-    const user = await User.findById(req.params.id);
-    return res.status(200).send({
-        message: user,
-    });
-}
+exports.getAllUserStudent = async (req, res) => {
+  const { page } = req.params;
+  const pageSize = 25;
+  const list = await User.find({ type: 'Người học' })
+    .skip(page * pageSize)
+    .limit(pageSize);
+  return res.status(200).send({
+    message: list,
+  });
+};
+exports.getNumberUserStudent = async (req, res) => {
+  const num = await User.count({ type: 'Người học' });
+  return res.status(200).send({
+    message: num,
+  });
+};
 
-exports.blockUser = async (req,res)=>{
-    const user = await User.findById(req.params.id);
-    if (user)
-    {
-        user.isBlocked = true;
-        await user.save();
-        res.status(200).send({
-            message: 'DOne',
-        });
-    } else {
-        res.status(500).send({
-            message: "Can't find user",
-        });
-    }
-    return res;
-}
+exports.getUserDetail = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  return res.status(200).send({
+    message: user,
+  });
+};
+
+exports.blockUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    user.isBlocked = true;
+    await user.save();
+    res.status(200).send({
+      message: 'DOne',
+    });
+  } else {
+    res.status(500).send({
+      message: "Can't find user",
+    });
+  }
+  return res;
+};
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 
 const router = express.Router();
-const accountController = require('../controllers/accountController') 
-const skillController = require('../controllers/skillController')
+const accountController = require('../controllers/accountController');
+const skillController = require('../controllers/skillController');
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/user-list/:page',accountController.getAllUser);
-router.get('/user-list/number',accountController.getNumberUser);
-router.get('/user-detail/:id',accountController.getUserDetail);
-router.get('/user/block/:id',accountController.blockUser);
+router.get('/user-list/teacher/:page', accountController.getAllUserTeacher);
+router.get('/user-list/teacher/number', accountController.getNumberUserTeacher);
+router.get('/user-list/student/:page', accountController.getAllUserStudent);
+router.get('/user-list/student/number', accountController.getNumberUserStudent);
 
-router.get('/skill-list/read',skillController.getSkill);
-router.post('/skill-list/create',skillController.createSkill);
-router.post('/skill-list/update',skillController.updateSkill);
+router.get('/user-detail/:id', accountController.getUserDetail);
+router.get('/user/block/:id', accountController.blockUser);
+
+router.get('/skill-list/read', skillController.getSkill);
+router.post('/skill-list/create', skillController.createSkill);
+router.post('/skill-list/update', skillController.updateSkill);
 router.post('/skill-list/delete', skillController.deleteSkill);
 
 module.exports = router;
