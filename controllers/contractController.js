c4215b4d55ca142282ddde57b01f370231ef504f
leuyennhi <1612470@student.hcmus.edu.vn>
2019-12-22 15:26:16 +0700

    update code

--- controllers/contractController.js
+++ controllers/contractController.js
@@ -1,4 +1,6 @@
 const Contract = require('../models/contracts');
+const Chat = require('../models/chats');
+const User = require('../models/users');
 
 exports.getContractList = async (req, res) => {
   const { page } = req.params;
@@ -13,4 +15,11 @@ exports.getContractList = async (req, res) => {
 exports.getNumContractList = async (req, res) => {
   const num = await Contract.countDocuments({ isDeleted: false });
   res.status(200).send({ message: num });
+};
+
+exports.getChat = async (req, res) => {
+  const result = await Chat.find({ studentID: req.body.studentID, teacherID: req.body.teacherID });
+  const teacherInfo = await User.findById(req.body.teacherID);
+  const studentInfo = await User.findById(req.body.studentID);
+  return res.status(200).send({ chat: result[0], teacher: teacherInfo, student: studentInfo });
 };
